ab21627bea51fb9ce44e2be39534cf248379956b
luming <lumingmelody@gmail.com>
2022-02-09 14:06:46 +0800

    update

--- xingtu_mcn.py
+++ xingtu_mcn.py
@@ -17,8 +17,7 @@ ws.append([
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
-    "cookie": "ttcid=a6090f76dd5c44fbbec4a168e40d834328; _tea_utm_cache_2018=undefined; MONITOR_DEVICE_ID=8c84e3d2-3e8b-4656-8c54-c5c2cb130771; tt_webid=7028866373770692127; csrftoken=fPGSy36I8BeD3WQbbMZ5CvvgnZrbK01c; passport_csrf_token_default=07aa241118dabb872e93c5ec839b6688; passport_csrf_token=07aa241118dabb872e93c5ec839b6688; _tea_utm_cache_1581=undefined; _tea_utm_cache_2925=undefined; MONITOR_WEB_ID=20edb3f9-47ce-4d74-910c-3b610af9e378; tt_scid=40ZuvR0Uh65qrApAvCSArpbtB.U6aWqy9nDx1LCLxlM4eAeXDEghWMyU5jCJqk7k43bd; s_v_web_id=verify_2a02307c944b749b3b8732a7d07c2cf0; uid_tt=487c48d51c8a293c72d2df360639dd53; uid_tt_ss=487c48d51c8a293c72d2df360639dd53; sid_tt=46013a8742ea58a290c398699ced54bb; sessionid=46013a8742ea58a290c398699ced54bb; sessionid_ss=46013a8742ea58a290c398699ced54bb; sid_ucp_v1=1.0.0-KDAxYTU5MGE3YmU1YTk2YzQ0NGE2NjBiNTNhMDQ1YmRkZDk0M2M1MjcKFQj6wvTWhwMQkpyyjAYY-hM4AUDrBxoCbGYiIDQ2MDEzYTg3NDJlYTU4YTI5MGMzOTg2OTljZWQ1NGJi; ssid_ucp_v1=1.0.0-KDAxYTU5MGE3YmU1YTk2YzQ0NGE2NjBiNTNhMDQ1YmRkZDk0M2M1MjcKFQj6wvTWhwMQkpyyjAYY-hM4AUDrBxoCbGYiIDQ2MDEzYTg3NDJlYTU4YTI5MGMzOTg2OTljZWQ1NGJi; sid_guard=46013a8742ea58a290c398699ced54bb%7C1636601362%7C5184000%7CMon%2C+10-Jan-2022+03%3A29%3A22+GMT; star_sessionid=d11fad1b3418a892797754a166a812b0; gftoken=NDYwMTNhODc0MnwxNjM2NjAxMzYzOTV8fDAGBgYGBgY",
-
+    "cookie": "ttcid=a6090f76dd5c44fbbec4a168e40d834328; tt_webid=7028866373770692127; csrftoken=fPGSy36I8BeD3WQbbMZ5CvvgnZrbK01c; MONITOR_WEB_ID=20edb3f9-47ce-4d74-910c-3b610af9e378; tt_scid=PKtaZ1hhrrV8P9Aa0lqxMGVzQZd66uEwc92JHuHAOKj87lnKfPQC6cDCmx.RnyKP78ba; s_v_web_id=verify_c4b49711dcfc8e4ed4c30637cfcd4cce; _tea_utm_cache_2018=undefined; MONITOR_DEVICE_ID=a6316cfd-9602-4267-a3f6-39ab328ad7d5; passport_csrf_token_default=3f66dd371a88187dc2a01febbe91a09a; sid_guard=43757967bd797f38e9c740dad96b01b9%7C1644372282%7C5184000%7CSun%2C+10-Apr-2022+02%3A04%3A42+GMT; uid_tt=b1a267c19c8c5ed4e49910e7ad6e56b6; uid_tt_ss=b1a267c19c8c5ed4e49910e7ad6e56b6; sid_tt=43757967bd797f38e9c740dad96b01b9; sessionid=43757967bd797f38e9c740dad96b01b9; sessionid_ss=43757967bd797f38e9c740dad96b01b9; sid_ucp_v1=1.0.0-KDZhZmZmYzBkYzIyZjU2MDYwMDQzODBlYjlkOTg2NGE1YjE1Y2UxMTMKFQj6wvTWhwMQusKMkAYY-hM4AUDrBxoCbGYiIDQzNzU3OTY3YmQ3OTdmMzhlOWM3NDBkYWQ5NmIwMWI5; ssid_ucp_v1=1.0.0-KDZhZmZmYzBkYzIyZjU2MDYwMDQzODBlYjlkOTg2NGE1YjE1Y2UxMTMKFQj6wvTWhwMQusKMkAYY-hM4AUDrBxoCbGYiIDQzNzU3OTY3YmQ3OTdmMzhlOWM3NDBkYWQ5NmIwMWI5; passport_csrf_token=3f66dd371a88187dc2a01febbe91a09a; _tea_utm_cache_1581=undefined; _tea_utm_cache_2925=undefined; star_sessionid=3619e9dba4cfb836563836c87b4c5cd1; gftoken=NDM3NTc5NjdiZHwxNjQ0MzcyMjg1NzB8fDAGBgYGBgY"
 }
 
 
@@ -40,7 +39,7 @@ def get_public_sign(mcn_id):
     return sign
 
 
-def get_pagetop_sign(mcn_id):
+def get_page_top_sign(mcn_id):
     # "mcn_id6685143170927296524service_methodMcnMainPageTopAuthorsservice_namemcn.AdStarMcnServicesign_strict1"
     m = hashlib.md5()
     str = f"mcn_id{mcn_id}service_methodMcnMainPageTopAuthorsservice_namemcn.AdStarMcnServicesign_strict1" + "e39539b8836fb99e1538974d3ac1fe98"
@@ -69,7 +68,7 @@ def get_mcn(pageNum):
         author_num = result['author_num']
         introduction = result['introduction']
         sum_follower = result['sum_follower']
-        page_top_sign = get_pagetop_sign(mcn_id)
+        page_top_sign = get_page_top_sign(mcn_id)
         page_top_url = f"https://www.xingtu.cn/h/api/gateway/handler_get/?mcn_id={mcn_id}&service_name=mcn.AdStarMcnService&service_method=McnMainPageTopAuthors&sign_strict=1&sign={page_top_sign}"
         res = requests.get(url=page_top_url, headers=headers).json()
         top_follower_authors = res['data']['top_follower_authors']
